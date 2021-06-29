641ae7cafe87c345f3fe93a15c2722b5099f8092
bhagyesh-webosmotic <bhagyesh.webosmotic@gmail.com>
2021-06-29 15:47:58 +0530

    edit data : mappind data back to form part 1

--- task 10/editData.js
+++ task 10/editData.js
@@ -4,9 +4,64 @@ function editFormData(e) {
   } else {
     let dataArray = [];
     dataArray = JSON.parse(localStorage["jsonform"]);
-    elementsArray.forEach((element) => {
-      let value = document.querySelector(`[key="${element.key}"]`).value;
-      obj[element.key] = value;
+    editId = e.target.id;
+    // console.log(e.target.id);
+    dataArray.forEach((LSelement) => {
+      if (LSelement.userId == editId) {
+        elementsArray.forEach((JSONelement) => {
+          var key = JSONelement.key;
+          if (
+            JSONelement.type != "submit" &&
+            JSONelement.type != "reset" &&
+            JSONelement.type != "radio" &&
+            JSONelement.type != "checkbox"
+          ) {
+            //retriving DOM input fields values from localstorage
+            document.querySelector(`[key="${JSONelement.key}"]`).value =
+              LSelement[key];
+          } else if (
+            JSONelement.type == "null" &&
+            JSONelement.key == "userId"
+          ) {
+            document.querySelector(`[key="${JSONelement.key}"]`).value =
+              LSelement[key];
+          } else if (
+            //for retriving radio value
+            JSONelement.type == "radio" &&
+            JSONelement.type != "submit" &&
+            JSONelement.type != "reset"
+          ) {
+            let ele = document.getElementsByName("gender");
+            for (i = 0; i < ele.length; i++) {
+              if (ele[i].value == LSelement[key]) {
+                ele[i].checked = true;
+              }
+            }
+          } else if (
+            //for retriving checkbox values
+            JSONelement.type == "checkbox" &&
+            JSONelement.type != "submit" &&
+            JSONelement.type != "reset"
+          ) {
+            let chkbxln = document.querySelectorAll(
+              `[key="${JSONelement.key}"]`
+            );
+            var k = 0;
+            for (i = 0; i < chkbxln.length; i++) {
+              if (chkbxln[i].value == LSelement[key][k]) {
+                chkbxln[i].checked = true;
+                k++;
+              }
+            }
+          } else if (
+            JSONelement.type == "null" &&
+            JSONelement.key == "createdAt"
+          ) {
+            document.querySelector(`[key="${JSONelement.key}"]`).value =
+              LSelement[key];
+          }
+        });
+      }
     });
   }
 }
--- task 10/saveData.js
+++ task 10/saveData.js
@@ -8,6 +8,15 @@ function saveFormData(e) {
     dataArray = JSON.parse(localStorage["jsonform"]);
     let obj = {};
     let chkboxArr = [];
+    console.log(document.querySelector(`[key="userId"]`).value);
+
+    dataArray.forEach((LSelement) => {
+      if (document.querySelector(`[key="userId"]`).value == LSelement.userId) {
+        console.log("matched data");
+      } else {
+        console.log("new data");
+      }
+    });
 
     elementsArray.forEach((element) => {
       if (
@@ -19,42 +28,41 @@ function saveFormData(e) {
         //for saving DOM input fields values
         let value = document.querySelector(`[key="${element.key}"]`).value;
         obj[element.key] = value;
-      } else {
-        if (
-          //for saving radio value
-          element.type == "radio" &&
-          element.type != "submit" &&
-          element.type != "reset"
-        ) {
-          let ele = document.getElementsByName("gender");
-          for (i = 0; i < ele.length; i++) {
-            if (ele[i].checked) {
-              obj[element.key] = ele[i].value;
-            }
+      } else if (
+        //for saving radio value
+        element.type == "radio" &&
+        element.type != "submit" &&
+        element.type != "reset"
+      ) {
+        let ele = document.getElementsByName("gender");
+        for (i = 0; i < ele.length; i++) {
+          if (ele[i].checked) {
+            obj[element.key] = ele[i].value;
           }
-        } else if (
-          //for saving checkbox values
-          element.type == "checkbox" &&
-          element.type != "submit" &&
-          element.type != "reset"
-        ) {
-          //   console.log(document.querySelectorAll(`[key="${element.key}"]`));
-          let chkbxln = document.querySelectorAll(`[key="${element.key}"]`);
-          for (i = 0; i < chkbxln.length; i++) {
-            if (chkbxln[i].checked) {
-              chkboxArr.push(chkbxln[i].value);
-            }
-          }
-          obj[element.key] = chkboxArr;
-        } else if (element.type == "null" && element.key == "userId") {
-          obj[element.key] = getRandomNumber();
-        } else if (element.type == "null" && element.key == "createdAt") {
-          var value = document.querySelector(`[key="${element.key}"]`).value;
-          console.log(value);
-          obj[element.key] = dateDisplay(value);
         }
+      } else if (
+        //for saving checkbox values
+        element.type == "checkbox" &&
+        element.type != "submit" &&
+        element.type != "reset"
+      ) {
+        //   console.log(document.querySelectorAll(`[key="${element.key}"]`));
+        let chkbxln = document.querySelectorAll(`[key="${element.key}"]`);
+        for (i = 0; i < chkbxln.length; i++) {
+          if (chkbxln[i].checked) {
+            chkboxArr.push(chkbxln[i].value);
+          }
+        }
+        obj[element.key] = chkboxArr;
+      } else if (element.type == "null" && element.key == "userId") {
+        obj[element.key] = getRandomNumber();
+      } else if (element.type == "null" && element.key == "createdAt") {
+        var value = document.querySelector(`[key="${element.key}"]`).value;
+        console.log(value);
+        obj[element.key] = dateDisplay(value);
       }
     });
+
     dataArray.push(obj);
     localStorage["jsonform"] = JSON.stringify(dataArray);
   }
