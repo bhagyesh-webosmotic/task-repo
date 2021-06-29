16eee234faf992b4d187765e45263e9f87a1b519
bhagyesh-webosmotic <bhagyesh.webosmotic@gmail.com>
2021-06-29 17:24:28 +0530

    added remove data feature

--- task 10/removeData.js
+++ task 10/removeData.js
@@ -1,7 +1,18 @@
 function removeFormData(e) {
-  var rmId = e.target.getAttribute("name");
-  dataArray = JSON.parse(localStorage["input"]);
-  dataArray.splice(rmId, 1);
-  localStorage["input"] = JSON.stringify(dataArray);
+  let dataArray = [];
+  dataArray = JSON.parse(localStorage["jsonform"]);
+  var rmId = e.target.id;
+  console.log(rmId);
+  dataArray.forEach((LSelement) => {
+    if (rmId == LSelement.userId) {
+      console.log("matched data for deletion");
+      const index = dataArray.indexOf(LSelement);
+      console.log(index);
+      dataArray.splice(index, 1);
+    } else {
+      console.log("no data found");
+    }
+  });
+  localStorage["jsonform"] = JSON.stringify(dataArray);
   displayFormInfo();
 }
